edcfb274616fb71f859bb466be97e20592c02817
TCB <tbinh2890@gmail.com>
2024-01-22 08:15:42 +0700

    update Bai4

--- Bai4/index.php
+++ Bai4/index.php
@@ -1,21 +1,39 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<?php
+    <?php
 
-$arr_number = array(3, 7, 6, 8, 9, 3, 7, 9, 0, 2, 6, 8, 9, 15, 70, 40, 120);
+    $arr_number = array(3, 7, 6, 8, 9, 3, 7, 9, 0, 2, 6, 8, 9, 15, 70, 40, 120);
+    echo "Mảng ban đầu: " . "<br />";
+    foreach ($arr_number as $n) {
+        echo "$n <br>";
+    }
+    echo "<br/>";
+    $arr_number_after = array_replace($arr_number, array_fill_keys(array_keys($arr_number, 9), 23));
+    echo "Mảng sau khi thay thế: " . "<br />";
+    foreach ($arr_number_after as $n) {
+        echo "$n <br>";
+    }
+    echo "<br/>";
+    echo "Mảng sắp xếp theo thứ tự tăng dần: " . "<br />";
+    sort($arr_number_after);
+    foreach ($arr_number_after as $n) {
+        echo "$n <br>";
+    }
+    echo "<br/>";
+    echo "Mảng sắp xếp theo thứ tự giảm dần: " . "<br />";
+    $arr_number_rsort = rsort($arr_number_after);
+    foreach ($arr_number_after as $n) {
+        echo "$n <br>";
+    }
+    echo "<br />";
+    echo "Tổng mảng cũ: " . array_sum($arr_number) . "<br />";
+    echo "Tổng mảng mới: " . array_sum($arr_number_after);
 
-$arr_number_after = array_replace($arr_number, array_fill_keys(array_keys($arr_number, 9), 23));
-
-print_r($arr_number);
-echo "<br />";
-print_r($arr_number_after);
-echo "<br />";
-echo "Tổng mảng cũ: " . array_sum($arr_number) . "<br />";
-echo "Tổng mảng mới: " . array_sum($arr_number_after);
-
-?>
+    ?>
 
 </body>
+
 </html>
